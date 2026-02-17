eb9e485c1e0700b03deb450eac6aa8b5ca6ee0e5
luisa <lucimart@iu.edu>
2026-02-16 23:06:11 -0500

    commit

---
+++ docs/app.py
@@ -0,0 +1,27 @@
+#app.py
+# This does the initiliazation and execution of the application
+# Uses Flask for it all.
+# Important note: Serves as the primary backend service for the the application
+# Delivers frontend pages as well.
+
+from flask import Flask, render_template
+
+def create_app():
+    app = Flask(__name__)
+
+    # Home route
+    @app.route("/")
+    def index():
+        return render_template("index.html")
+
+    # Calendar route
+    @app.route("/calendar")
+    def calendar_view():
+        return render_template("calendar.html")
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
